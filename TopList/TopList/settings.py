eeac96f65d1b1a42869a1f40e173afe553b87cb6
mihailovivan <mikhailovj@gmail.com>
2019-08-27 16:39:06 +0300

    backetname added

--- TopList/TopList/settings.py
+++ TopList/TopList/settings.py
@@ -149,10 +149,10 @@ STATIC_ROOT = './static_files/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'toplist'
 
 if os.getenv('USE_MINIO', False):
     AWS_ACCESS_KEY_ID = os.getenv('MINIO_STORAGE_ACCESS_KEY')
     AWS_SECRET_ACCESS_KEY = os.getenv('MINIO_STORAGE_SECRET_KEY')
     AWS_S3_ENDPOINT_URL = 'http://0.0.0.0:9000'
-    AWS_STORAGE_BUCKET_NAME = 'toplist'
     TEST_SERVER_MODE = True
